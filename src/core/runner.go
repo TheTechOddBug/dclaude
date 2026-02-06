767e92cc7c7325a08f8b642b81dd9b5409021c85
Patrick Debois <70908+jedi4ever@users.noreply.github.com>
2026-02-06 13:49:46 +0100

    runner debugging

--- src/core/runner.go
+++ src/core/runner.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 
 	"github.com/jedi4ever/addt/provider"
+	"github.com/jedi4ever/addt/util"
 )
+
+var runnerLogger = util.Log("runner")
 
 // Runner coordinates container execution
 type Runner struct {
@@ -22,30 +25,55 @@ func NewRunner(p provider.Provider, cfg *provider.Config) *Runner {
 
 // Run executes the container with the configured extension
 func (r *Runner) Run(args []string) error {
+	runnerLogger.Debugf("Runner.Run called with args: %v", args)
 	return r.execute(args, false)
 }
 
 // Shell opens an interactive shell in the container
 func (r *Runner) Shell(args []string) error {
+	runnerLogger.Debugf("Runner.Shell called with args: %v", args)
 	return r.execute(args, true)
 }
 
 // execute is the common execution logic for Run and Shell
 func (r *Runner) execute(args []string, openShell bool) error {
+	runnerLogger.Debugf("execute called with args: %v", args)
+	runnerLogger.Debugf("Runner.execute called: openShell=%v, args=%v", openShell, args)
+
 	// Determine container name
+	runnerLogger.Debug("Generating container name")
 	name := r.generateName()
+	runnerLogger.Debugf("Generated container name: %s", name)
 
 	// Build run options
+	runnerLogger.Debug("Building run options")
 	opts := BuildRunOptions(r.provider, r.config, name, args, openShell)
+	runnerLogger.Debugf("Run options: Name=%s, ImageName=%s, Args=%v, Interactive=%v, Persistent=%v",
+		opts.Name, opts.ImageName, opts.Args, opts.Interactive, opts.Persistent)
 
 	// Display status
+	runnerLogger.Debug("Displaying status")
 	DisplayStatus(r.provider, r.config, name)
 
 	// Execute via provider
 	if openShell {
-		return r.provider.Shell(opts)
+		runnerLogger.Debug("Calling provider.Shell")
+		err := r.provider.Shell(opts)
+		if err != nil {
+			runnerLogger.Errorf("Provider.Shell failed: %v", err)
+		} else {
+			runnerLogger.Debug("Provider.Shell completed successfully")
+		}
+		return err
 	}
-	return r.provider.Run(opts)
+	runnerLogger.Debug("Calling provider.Run")
+	err := r.provider.Run(opts)
+	if err != nil {
+		runnerLogger.Errorf("Provider.Run failed: %v", err)
+	} else {
+		runnerLogger.Debug("Provider.Run completed successfully")
+	}
+	return err
 }
 
 // generateName generates the container name based on persistence mode
